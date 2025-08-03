18409dacb556eda4e0a7a4ef1adaec13b89bd9b5
ValeriiTsoi <144263401+ValeriiTsoi@users.noreply.github.com>
2025-08-03 11:33:52 +0200

    Update 28 TestController.java

--- src/main/java/org/example/sbdemo/controller/TestController.java
+++ src/main/java/org/example/sbdemo/controller/TestController.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.*;
 public class TestController {
 
     @GetMapping
-    public ResponseEntity<String> getText(@RequestParam(required = false) {
+    public ResponseEntity<String> getText(@RequestParam(required = false) boolean show) {
         if (Boolean.TRUE.equals(show)) {
             return ResponseEntity.ok("\n This is test response! \n");
         } else {
-//            return ResponseEntity.ok("\n Params are not correct. use: //test?show=true \n");
-            return ResponseEntity.noContent().build();
+            return ResponseEntity.ok("\n Params are not correct. use: //test?show=true \n");
+//            return ResponseEntity.noContent().build();
         }
     }
 }
